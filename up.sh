29d0dfb4602b8ced168fa5b0a4aa8f574018632f
ryoichi-obara <ryoichi-obara@users.noreply.github.com>
2018-12-04 12:38:57 +0900

    :recycle: Improve up.sh. :recycle:

--- up.sh
+++ up.sh
@@ -1,10 +1,19 @@
 #/bin/sh
 
-npm run make
+# function for setting local.
+function main() {
+  local S3="【S3バケット名】"
+  local ZIP="index.zip"
+  #local PROF="【AWSプロファイル】"
+  local FUNC="【Lambda名】"
 
-# 再度npm iが必要ない場合はこちらを使ってもよい
-# rm ./build/Release/index.zip
-# npm run quick
+  npm run make
+  # 再度npm iが必要ない場合はこちらを使ってもよい
+  rm ./build/Release/index.zip
+  # npm run quick
 
-aws s3 cp ./build/Release/index.zip s3://【S3バケット名】/
-aws lambda update-function-code --function-name 【Lambda名】 --s3-bucket 【S3バケット名】 --s3-key index.zip --publish
+  aws s3 cp ./build/Release/index.zip s3://$S3/$ZIP
+  aws lambda update-function-code --function-name $FUNC --s3-bucket $S3 --s3-key $ZIP --publish
+}
+
+main
